2640b8ede16df100783b0ce94c8a4e702a0363d0
George Stavrinos <gstavrinos@protonmail.com>
2021-07-11 13:36:13 +0300

    Added launch file for description and joint states

---
+++ launch/description.launch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+    urdf_file_name = "custom_iris.xacro"
+
+    print("urdf_file_name : {}".format(urdf_file_name))
+
+    urdf = os.path.join(
+        get_package_share_directory("uav_inspections_ros2"),"urdf",
+        urdf_file_name)
+
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulation (Gazebo) clock if true"),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
+            parameters=[{"use_sim_time": use_sim_time, "robot_description" : robot_desc}]),
+
+        Node(
+            package="joint_state_publisher",
+            executable="joint_state_publisher",
+            name="joint_state_publisher",
+            output="screen"),
+    ])
+
