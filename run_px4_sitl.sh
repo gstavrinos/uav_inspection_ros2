ec6e7a3cc234b3a09fca79f8f6f532b489961346
George Stavrinos <gstavrinos@protonmail.com>
2021-07-13 22:07:52 +0300

    Changes that do not appear to make any meaningful difference

--- run_px4_sitl.sh
+++ run_px4_sitl.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+starting_dir=$pwd
 model=iris
 src_path=~/sources/PX4-Autopilot
 build_path=$src_path/build/px4_sitl_rtps
-world=$src_path/Tools/sitl_gazebo/worlds/empty.world
+world=$src_path/Tools/sitl_gazebo/worlds/iris.world
 urdf=~/ros2_ws/src/uav_inspections_ros2/urdf/custom_iris.xacro
 
 rootfs=$build_path/tmp/rootfs # this is the working directory
 mkdir -p $rootfs
+
+cd $rootfs
 
 cp $src_path/Tools/posix_lldbinit $rootfs/.lldbinit
 cp $src_path/Tools/posix.gdbinit $rootfs/.gdbinit
@@ -39,7 +42,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/sources/PX4-Autopilot/build/px4_sitl_r
 # gzserver --verbose $world &
 gzserver --verbose &
 
-while gz model --verbose --spawn-file=$urdf --model-name=$model -x 1.01 -y 0.98 -z 0.83 2>&1 | grep -q "An instance of Gazebo is not running."; do
+while gz model --verbose --spawn-file=$urdf --model-name=$model -x 0 -y 0 -z 1 2>&1 | grep -q "An instance of Gazebo is not running."; do
     echo "gzserver not ready yet, trying again!"
     sleep 1
 done
@@ -47,10 +50,15 @@ done
 sleep 5
 nice -n 20 gzclient --verbose &
 
+pushd $rootfs >/dev/null
+
 ~/sources/PX4-Autopilot/build/px4_sitl_rtps/bin/px4 ~/sources/PX4-Autopilot/build/px4_sitl_rtps/etc -s etc/init.d-posix/rcS -t ~/sources/PX4-Autopilot/test_data
+
+popd >/dev/null
 
 pkill -x gazebo || true
 pkill -x gzserver || true
 pkill -x gzclient || true
 pkill -x px4 || true
 pkill -x px4_$model || true
+cd $starting_dir
