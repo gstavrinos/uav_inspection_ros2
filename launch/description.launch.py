5a7a9cc9be25cf3e3f11ef180dfe5efffccc5482
George Stavrinos <gstavrinos@protonmail.com>
2021-07-16 02:53:52 +0300

    Updated the launch file to support simulated environments, for which it disables the joint_state_publisher

--- launch/description.launch.py
+++ launch/description.launch.py
@@ -9,7 +9,8 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+    simulation = LaunchConfiguration("simulation", default="false")
+    use_sim_time = LaunchConfiguration("use_sim_time", default=simulation)
     urdf_file_name = "custom_iris.xacro"
 
     print("urdf_file_name : {}".format(urdf_file_name))
@@ -21,7 +22,7 @@ def generate_launch_description():
     with open(urdf, 'r') as infp:
         robot_desc = infp.read()
 
-    return LaunchDescription([
+    ld = [
         DeclareLaunchArgument(
             "use_sim_time",
             default_value="false",
@@ -32,12 +33,13 @@ def generate_launch_description():
             executable="robot_state_publisher",
             name="robot_state_publisher",
             output="screen",
-            parameters=[{"use_sim_time": use_sim_time, "robot_description" : robot_desc}]),
-
-        Node(
-            package="joint_state_publisher",
-            executable="joint_state_publisher",
-            name="joint_state_publisher",
-            output="screen"),
-    ])
+            parameters=[{"use_sim_time": use_sim_time, "robot_description" : robot_desc}])
+        ]
+    if not simulation:
+            ld.append(Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen"))
+    return LaunchDescription(ld)
 
