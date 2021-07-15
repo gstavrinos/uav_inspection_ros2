f892060f2735d4a1d1f103dfee22288d7af6fd7c
George Stavrinos <gstavrinos@protonmail.com>
2021-07-16 01:48:55 +0300

    Minor fixes on the execution script. I might add some parameters for quicker changes

--- run_px4_sitl.sh
+++ run_px4_sitl.sh
@@ -6,6 +6,8 @@ src_path=~/sources/PX4-Autopilot
 build_path=$src_path/build/px4_sitl_rtps
 world=$src_path/Tools/sitl_gazebo/worlds/empty.world
 urdf=~/ros2_ws/src/uav_inspections_ros2/urdf/custom_iris.xacro
+# Bypass the urdf with the original sdf for debugging
+# urdf=~/sources/PX4-Autopilot/Tools/sitl_gazebo/models/iris/iris.sdf
 
 rootfs=$build_path/tmp/rootfs # this is the working directory
 mkdir -p $rootfs
@@ -19,13 +21,6 @@ for file in "$@"; do
     cp "$file" $rootfs/
 done
 
-# To disable user input
-# if [[ -n "$NO_PXH" ]]; then
-    # no_pxh=-d
-# else
-    # no_pxh=""
-# fi
-
 pkill -x gazebo || true
 pkill -x gzserver || true
 pkill -x gzclient || true
@@ -34,13 +29,14 @@ pkill -x px4_$model || true
 
 export PX4_SIM_MODEL=$model
 . ~/.bashrc
-. /usr/share/gazebo-11/setup.sh
+# . /usr/share/gazebo-11/setup.sh
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:~/sources/PX4-Autopilot/build/px4_sitl_rtps/build_gazebo
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/sources/PX4-Autopilot/Tools/sitl_gazebo/models:~/ros2_ws/src/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/sources/PX4-Autopilot/build/px4_sitl_rtps/build_gazebo
-
-# gzserver --verbose $world &
-gzserver --verbose &
+# echo $GAZEBO_MODEL_PATH
+# echo $GAZEBO_PLUGIN_PATH
+# echo $LD_LIBRARY_PATH
+gzserver --verbose $world &
 
 while gz model --verbose --spawn-file=$urdf --model-name=$model -x 1.01 -y 0.98 -z 0.83 2>&1 | grep -q "An instance of Gazebo is not running."; do
     echo "gzserver not ready yet, trying again!"
