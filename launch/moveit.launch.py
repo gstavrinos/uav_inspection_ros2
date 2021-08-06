83e1b5b6ff8eba54cc63bcd5b5acd28637e1f9ea
George Stavrinos <stavrinosgeo@gmail.com>
2021-08-06 08:49:32 +0300

    static tf odom->base_link instead of world->odom (is this hacky?)

--- launch/moveit.launch.py
+++ launch/moveit.launch.py
@@ -156,7 +156,7 @@ def generate_launch_description():
         executable="static_transform_publisher",
         name="static_transform_publisher",
         output="log",
-        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "odom"],
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "odom", "base_link"],
     )
 
     # Publish TF
