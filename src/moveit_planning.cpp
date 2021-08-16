5cf1a83ef721a3d608bf983c635a329428c626ce
George Stavrinos <stavrinosgeo@gmail.com>
2021-08-16 15:50:11 +0300

    Simple moveit (movegroup) template to build on top

--- src/moveit_planning.cpp
+++ src/moveit_planning.cpp
@@ -1,74 +1,72 @@
 #include <thread>
 #include <rclcpp/rclcpp.hpp>
-#include <moveit/moveit_cpp/moveit_cpp.h>
-#include <moveit/robot_state/conversions.h>
-#include <moveit/moveit_cpp/planning_component.h>
-#include <moveit_msgs/msg/display_robot_state.hpp>
-#include <trajectory_msgs/msg/joint_trajectory.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("uav_moveit");
 
 class MoveItPlanning {
 public:
-  MoveItPlanning(const rclcpp::Node::SharedPtr& node)
-    : node_(node)
-    , robot_state_publisher_(node_->create_publisher<moveit_msgs::msg::DisplayRobotState>("display_robot_state", 1))
-  {
-  }
+    MoveItPlanning(const rclcpp::Node::SharedPtr& node) : node_(node) {
+        move_group_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(node_, PLANNING_GROUP);
+        move_group_->setPlanningTime(PLANNING_TIME_S);
+        move_group_->setNumPlanningAttempts(PLANNING_ATTEMPTS);
+        ref_link_ = move_group_->getPoseReferenceFrame();
+    }
 
-  void run() {
-    RCLCPP_INFO(LOGGER, "Initialize MoveItCpp");
-    moveit_cpp_ = std::make_shared<moveit_cpp::MoveItCpp>(node_);
-    moveit_cpp_->getPlanningSceneMonitor()->providePlanningSceneService();  // let RViz display query PlanningScene
-    moveit_cpp_->getPlanningSceneMonitor()->setPlanningScenePublishingFrequency(100);
+void run() {
+    move_group_->clearPoseTargets();
+    move_group_->clearPathConstraints();
+    move_group_->setStartStateToCurrentState();
 
-    RCLCPP_INFO(LOGGER, "Initialize PlanningComponent");
-    moveit_cpp::PlanningComponent uav("uav", moveit_cpp_);
+    geometry_msgs::msg::PoseStamped goal_pose;
+    goal_pose.header.stamp = node_->now();
+    goal_pose.header.frame_id = ref_link_;
+    goal_pose.pose.position.x = 0.5;
+    goal_pose.pose.position.y = -0.25;
+    goal_pose.pose.position.z = 1.0;
+    goal_pose.pose.orientation.x = 0.707;
+    goal_pose.pose.orientation.w = 0.707;
 
-    // A little delay before running the plan
-    rclcpp::sleep_for(std::chrono::seconds(3));
+    move_group_->setPoseTarget(goal_pose);
 
-    // Set joint state goal
-    RCLCPP_INFO(LOGGER, "Set goal");
-    uav.setGoal("extended");
-
-    // Run actual plan
-    RCLCPP_INFO(LOGGER, "Plan to goal");
-    const auto plan_solution = uav.plan();
-    // if (plan_solution)
-    // {
-      // RCLCPP_INFO(LOGGER, "uav.execute()");
-      // uav.execute();
-    // }
-  }
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    const bool plan_success = (move_group_->plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    RCLCPP_INFO(LOGGER, "Plan %s", plan_success ? "SUCCEEDED" : "FAILED");
+}
 
 private:
-  rclcpp::Node::SharedPtr node_;
-  rclcpp::Publisher<moveit_msgs::msg::DisplayRobotState>::SharedPtr robot_state_publisher_;
-  moveit_cpp::MoveItCppPtr moveit_cpp_;
+    std::string ref_link_;
+    const double PLANNING_TIME_S = 30.0;
+    const double PLANNING_ATTEMPTS = 5.0;
+    const std::string PLANNING_GROUP = "uav";
+    rclcpp::Node::SharedPtr node_;
+    moveit::planning_interface::MoveGroupInterfacePtr move_group_;
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
 };
 
-int main(int argc, char** argv)
-{
-  RCLCPP_INFO(LOGGER, "Initialize node");
-  rclcpp::init(argc, argv);
-  rclcpp::NodeOptions node_options;
-  // This enables loading undeclared parameters
-  // best practice would be to declare parameters in the corresponding classes
-  // and provide descriptions about expected use
-  node_options.automatically_declare_parameters_from_overrides(true);
-  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("uav_moveit", "", node_options);
+int main(int argc, char** argv) {
+    RCLCPP_INFO(LOGGER, "Initializing node");
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    // This enables loading undeclared parameters
+    // best practice would be to declare parameters in the corresponding classes
+    // and provide descriptions about expected use
+    node_options.automatically_declare_parameters_from_overrides(true);
+    rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("uav_moveit", "", node_options);
 
-  MoveItPlanning uav_moveit(node);
-  std::thread moveit_thread([&uav_moveit]() {
-    // Let RViz initialize before running demo
-    // TODO(henningkayser): use lifecycle events to launch node
-    rclcpp::sleep_for(std::chrono::seconds(5));
-    uav_moveit.run();
-  });
+    MoveItPlanning uav_moveit(node);
+    std::thread moveit_thread([&uav_moveit]() {
+        rclcpp::sleep_for(std::chrono::seconds(10));
+        uav_moveit.run();
+    });
 
-  rclcpp::spin(node);
-  moveit_thread.join();
+    rclcpp::spin(node);
+    moveit_thread.join();
 
-  return 0;
+    rclcpp::shutdown();
+
+    return 0;
 }
+
