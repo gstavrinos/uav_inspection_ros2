df0b0064b14335c167cf120053331fa522c90c4a
George Stavrinos <stavrinosgeo@gmail.com>
2021-09-21 15:34:46 +0300

    Mainly minor and naming fixes

--- src/moveit_planning.cpp
+++ src/moveit_planning.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <thread>
 #include <tf2_ros/buffer.h>
 #include <rclcpp/rclcpp.hpp>
@@ -16,16 +17,18 @@ static const rclcpp::Logger LOGGER = rclcpp::get_logger("uav_moveit");
 
 class MoveItPlanning : public rclcpp::Node {
 public:
-    MoveItPlanning(std::string name, rclcpp::NodeOptions node_options, rclcpp::Node::SharedPtr move_group_node) : Node(name, node_options) {
+    MoveItPlanning(std::string name, rclcpp::NodeOptions node_options, std::shared_ptr<rclcpp::Node> move_group_node) : Node(name, node_options) {
         node_ = move_group_node;
-        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(node_->get_clock());
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
         move_group_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(node_, PLANNING_GROUP);
+        move_group_->setPlannerId("RRTConnectkConfigDefault");
         move_group_->setPlanningTime(PLANNING_TIME_S);
         move_group_->setNumPlanningAttempts(PLANNING_ATTEMPTS);
+
         ref_link_ = move_group_->getPoseReferenceFrame();
         goal_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("simple_goal_publisher/goal", 10);
-        posestamped_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("moveit_planning/goal", 10, std::bind(&MoveItPlanning::poseStampedCallback, this, std::placeholders::_1));
+        posestamped_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("uav_moveit/goal", 10, std::bind(&MoveItPlanning::poseStampedCallback, this, std::placeholders::_1));
         timer_ = this->create_wall_timer(0.1s, [this]() {
                 planning();
             });
@@ -73,8 +76,8 @@ void MoveItPlanning::planning() {
         joint_group_positions[4] = moveit_goal.pose.orientation.y;
         joint_group_positions[5] = moveit_goal.pose.orientation.z;
         joint_group_positions[6] = moveit_goal.pose.orientation.w;
-        move_group_->setJointValueTarget(joint_group_positions);
         RCLCPP_INFO(LOGGER, "Planning....");
+        move_group_->setJointValueTarget(joint_group_positions);
         const bool plan_success = (move_group_->plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
         currently_planning = false;
         RCLCPP_INFO(LOGGER, "=================================================================");
@@ -135,12 +138,13 @@ int main(int argc, char** argv) {
     // best practice would be to declare parameters in the corresponding classes
     // and provide descriptions about expected use
     node_options.automatically_declare_parameters_from_overrides(true);
-    rclcpp::Node::SharedPtr move_group_node = rclcpp::Node::make_shared("moveit_planning_move_group_helper_node", node_options);
-    rclcpp::executors::SingleThreadedExecutor executor;
+    rclcpp::Node::SharedPtr move_group_node = rclcpp::Node::make_shared("uav_moveit_move_group_helper_node", node_options);
+    rclcpp::executors::MultiThreadedExecutor executor;
+    // rclcpp::executors::SingleThreadedExecutor executor;
     executor.add_node(move_group_node);
     std::thread([&executor]() { executor.spin(); }).detach();
 
-    rclcpp::spin(std::make_shared<MoveItPlanning>("moveit_planning", node_options, move_group_node));
+    rclcpp::spin(std::make_shared<MoveItPlanning>("uav_moveit", node_options, move_group_node));
 
     rclcpp::shutdown();
     return 0;
